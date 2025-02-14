bcadd67945ed5bc9e7b38fae06f9b1cb809d875d
ishva <ivekariya27@gmail.com>
2025-02-14 19:02:34 +0530

    add files

---
+++ assets/java/index.js
@@ -0,0 +1,16 @@
+function two() {
+    document.getElementById("content-1").style.display="block";
+    document.getElementById("content").style.display="none";
+    document.getElementById("two").style.backgroundColor="#DABA5D";
+    document.getElementById("two").style.color="#fff";
+    document.getElementById("one").style.backgroundColor="#F8F8F8";
+    document.getElementById("one").style.color="#666666";
+}
+function one() {
+    document.getElementById("content-1").style.display="none";
+    document.getElementById("content").style.display="block";
+    document.getElementById("one").style.backgroundColor="#DABA5D";
+    document.getElementById("one").style.color="#fff";
+    document.getElementById("two").style.backgroundColor="#F8F8F8";
+    document.getElementById("two").style.color="#666666";
+}
